092979c183eb3fbeef2f8152d76f68fa8131602a
srana <srana6@bloomberg.net>
2018-11-21 16:42:07 -0500

    Update the color token name to semantic css names

--- scripts/generate_color_css.js
+++ scripts/generate_color_css.js
@@ -2,7 +2,9 @@ const _ = require('lodash');
 const {stylesheet, ruleset} = require('./stylesheet-util');
 const colorTokens = require('../dist/index.common');
 
-const colorList = () => _.map(colorTokens, (tokenValue, tokenName) => [tokenName, tokenValue]);
+const cssTokenName = (tokenName) => `--${_.kebabCase(tokenName)}`
+
+const colorList = () => _.map(colorTokens, (tokenValue, tokenName) => [cssTokenName(tokenName), tokenValue]);
 
 const colorClassName = (colorName) => `${_.camelCase(colorName)}`;
 
